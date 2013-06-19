35c6ad88dda363b978f2eb452978bbd82872a512
raul guiu <raulguiu@gmail.com>
2013-06-19 19:52:59 +0200

    Ultimos cambios hechos en directo en clase

--- leccion_5/ejercicios/js/ejercicio_2.js
+++ leccion_5/ejercicios/js/ejercicio_2.js
@@ -1,53 +1,53 @@
 var repo_url = "/rguiu/curso_juegos"
 
-var game_request = new XMLHttpRequest();
-// var canvas = null;
-// var context = null;
+var github_request = new XMLHttpRequest();
 
-
-parseJSON = function (json_data) {
-    parsedJSON = JSON.parse(json_data);
-    console.log(parsedJSON[0].commit.message);
-    console.log(parsedJSON[0]);
-};
+var noimagen = "http://bit.ly/noimagen";
 
 main = function() {
 	update_ajax();
-	// canvas    = document.getElementById('canvas');
-	// context   = canvas.getContext('2d');
-	// canvas.width  = window.innerWidth;
-	// canvas.height = window.innerHeight - 200;
-
+	setInterval(update_ajax, 5000);
 };
 
 update_ajax = function() {
-	// para abrir una conexion pasamos los siguientes argumentos
-// 1. el metodo HTTP a usar, en este caso GET
-// 2. el recurso a cargar
-// 3. un booleano para describir la llamada como sincrona o asincrona
-	game_request.open("GET","https://api.github.com/repos"+repo_url+"/commits", true);
-    game_request.onload = function() {return parseJSON(this.responseText)};
-	game_request.send();
+	github_request.open("GET","https://api.github.com/repos"+repo_url+
+		"/commits", true);
+  github_request.onload = function() {
+  	display_commits(JSON.parse(this.responseText));
+  };
+	github_request.send();
 }
 
-paint = function(commits) {
-	var y = 5;
-	for(var i = 0;i < commits.length;i++) {
-		var img = new Img
-		context.beginPath();
-	    context.drawImage(x,y, 30, 45);
-	    context.fillStyle = '#dddddd';
-	    context.fill();
-	    context.fillStyle = '#000000';
-	    context.lineWidth = 1;
-	    context.strokeStyle = 'black';
-	    context.stroke();
-	    context.restore();
+display_commits = function(commits) {
+	var table = document.getElementById("commit_data");
+	table.innerHTML = "";
+	var tbdy = document.createElement("tbody");
+	table.appendChild(tbdy);
 
-	    context.font = "bold 15px Arial";
-	    context.fillText(rank, x+2, y+26);
-	    context.restore();
+	for(var i=0; i<commits.length;i++) {
+		var tr=document.createElement("tr");
+		var tdimagen = document.createElement("td");
+		var avatar = document.createElement("img");
+		if (commits[i].author == null) {
+			avatar.setAttribute("src", noimagen);
+		} else {
+			avatar.setAttribute("src", commits[i].author.avatar_url);
+		}
+		tdimagen.appendChild(avatar);
+		tr.appendChild(tdimagen);
+
+		var a = document.createElement("a");
+		var linkText = document.createTextNode(commits[i].commit.message);
+		a.appendChild(linkText);
+		a.href=commits[i].html_url;
+
+		var tdlink = document.createElement("td");
+		tdlink.appendChild(a);
+
+		tr.appendChild(tdlink);
+		tbdy.appendChild(tr);
 	}
+
 };
 
 window.onload = main;
